022b2059cda5b3abe86c86f8b2b9e900776a3df4
muhang <magicmuhan@gmail.com>
2019-01-19 11:59:15 -0500

    project skeleton

--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+  "html/template"
+  "net/http"
+  "path/filepath"
+  "log"
+  "regexp"
+)
+
+var templates = template.Must(template.ParseFiles("templates/index.html"))
+var validPath = regexp.MustCompile("^/$")
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request)) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path == "/" {
+      fn(w, r)
+      return
+    }
+
+    m := validPath.FindStringSubmatch(r.URL.Path)
+
+    if m == nil {
+      notFoundHandler(w, r)
+      return
+    }
+
+    fn(w, r)
+  }
+}
+
+func renderTemplate(w http.ResponseWriter, t string) {
+  lp := filepath.Join("templates", "layout.html")
+  fp := filepath.Join("templates", t+".html")
+
+  tmpl, err := template.ParseFiles(lp, fp)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+
+  tmpl.ExecuteTemplate(w, "layout", nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  renderTemplate(w, "index")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+  renderTemplate(w, "notFound")
+}
 
 func main() {
-        fmt.Println("todo")
+        fs := http.FileServer(http.Dir("static"))
+        http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+        http.HandleFunc("/", makeHandler(indexHandler))
+
+        log.Println("Listening...")
+        log.Fatal(http.ListenAndServe(":8080", nil))
 }
